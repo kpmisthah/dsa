4bbc64ff8d4f3c86eb88d9d7b0fdb8facafe817c
Misthah <kpmisthah6@gmail.com>
2025-03-14 14:26:02 +0530

    priority

--- DSA 3/Heap/priorityQueue.js
+++ DSA 3/Heap/priorityQueue.js
@@ -1,9 +1,9 @@
-class Minheap{
+class PriorityQueue{
     constructor(){
         this.heap = []
     }
-    push(value){
-        this.heap.push(value)
+    enqueue(value,priority){
+        this.heap.push({value,priority})
         this.heapifyUp()
     }
     pop(){
@@ -13,20 +13,17 @@ class Minheap{
             this.heap[0] = end
         }
         this.heapifyDown()
-    }
-    peek(){
-        return this.heap[0]
+        return start
     }
     heapifyUp(){
         let index = this.heap.length-1
         let parentIndex = Math.floor((index-1)/2)
-        while(index>0 && this.heap[parentIndex]>this.heap[index]){
+        while(index>0 && this.heap[parentIndex].priority>this.heap[index].priority){
             [this.heap[index],this.heap[parentIndex]] = [this.heap[parentIndex],this.heap[index]]
             index = parentIndex
             parentIndex = Math.floor((index-1)/2)
         }
     }
-    
     heapifyDown(){
         let index = 0
         let length = this.heap.length
@@ -34,26 +31,35 @@ class Minheap{
             let left = 2*index+1
             let right = 2*index+2
             let smallest = index
-            if(left<length && this.heap[left]<this.heap[smallest]){
+            if(left<length && this.heap[left].priority<this.heap[smallest].priority){
                 smallest = left
             }
-            if(right<length && this.heap[right]<this.heap[smallest]){
+            if(right<length && this.heap[right].priority<this.heap[smallest]){
                 smallest = right
             }
-            if(smallest == index) break
+            if(smallest == index)break
             [this.heap[index],this.heap[smallest]] = [this.heap[smallest],this.heap[index]]
             index = smallest
         }
     }
+    
     getHeap(){
         return this.heap
     }
+    
+    peek(){
+        return this.heap[0]
+    }
+    
+    isEmpty(){
+        return this.heap.length==0
+    }
 }
 
-let heap = new Minheap()
-heap.push(40)
-heap.push(420)
-heap.push(330)
-heap.push(10)
-heap.push(60)
-console.log(heap.getHeap())
+let pq = new PriorityQueue()
+pq.enqueue("Task 1", 3);  
+pq.enqueue("Task 2", 1);
+pq.enqueue("Task 3", 4);
+pq.enqueue("Task 4", 2);
+pq.enqueue("Task 5", 5);
+console.log(pq.getHeap())
