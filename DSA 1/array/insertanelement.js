8c4ea7035c6e5bab4d234791644ec8b65a0162e5
kpmisthah <kpmisthah6@gmail.com>
2026-03-03 22:17:58 +0530

    Update insertanelement.js

--- DSA 1/array/insertanelement.js
+++ DSA 1/array/insertanelement.js
@@ -10,11 +10,5 @@
 // function arr(arr,pos,num){
 //     let size = arr.length
 
-//     for(let i = size ; i>pos;i--){
-//         arr[i] = arr[i-1]
-//     }
-//     arr[pos] = num
-//     return arr
-// }
-// console.log(arr([1,2,3,4,5,6],2,8))
+
 
