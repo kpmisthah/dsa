936b109de35c8a7f0a64902ca87240292e0d80c0
kpmisthah <kpmisthah6@gmail.com>
2026-03-03 22:18:13 +0530

    Update insertanelement.js

--- DSA 1/array/insertanelement.js
+++ DSA 1/array/insertanelement.js
@@ -1,14 +1,2 @@
-//  Insert an Element: Write a function to insert an element at a specific position in an array
-
-// function arr(arr,i,num){
-//     arr.splice(i,0,num)
-//     return arr
-// }
-// console.log(arr([1,2,3,4,5,6],2,3))
-//................................................
-//without splice
-// function arr(arr,pos,num){
-//     let size = arr.length
-
 
 
