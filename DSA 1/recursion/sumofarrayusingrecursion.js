1be257093f7c7817f64b883477d635ebd29dc8f8
kpmisthah <kpmisthah6@gmail.com>
2026-03-23 09:14:26 +0530

    Update sumofarrayusingrecursion.js

--- DSA 1/recursion/sumofarrayusingrecursion.js
+++ DSA 1/recursion/sumofarrayusingrecursion.js
@@ -1,11 +1,3 @@
-// function sum(array){
-//     if(array.length<1){
-//         return 0
-//     }
-//     return array.pop()+sum(array)
-// }
-
-// console.log(sum([2,3,4,5,6,7]))
 
 function sum(array,index=0){
     if(index == array.length) return 0
